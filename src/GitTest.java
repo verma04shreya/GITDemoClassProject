fd8cb73231722fc1952fc253cf28b026583a6b18
dell <shreyaverma0403@gmail.com>
2022-02-04 15:23:19 +0530

    Added two lines

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,7 @@ public class GitTest {
 
     public static void main(String args[]){
         System.out.println("Helllo world!");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
     }
 }
